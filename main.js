b3146b15f2fcd178644da54c48febe3b1400ee5c
Grzegorz <g.juzba@gmail.com>
2020-08-19 12:48:57 +0200

    create main.js
    add html elements to main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+const inputAdd = document.querySelector('.add input');
+const addBtn = document.querySelector('.add button');
+const inputSearch = document.querySelector('.search');
+const taskCounter = document.querySelector('h1 span');
+const tasksList = document.querySelector('ul');
