c281b458c3b7e5991cd67b4eaa9ccebb0420a00c
Grzegorz <g.juzba@gmail.com>
2020-08-19 14:13:21 +0200

    removeTask function added

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const inputAdd = document.querySelector('.add input');
 const inputSearch = document.querySelector('.search');
 let taskCounter = document.querySelector('h1 span');
 const tasksList = document.querySelector('ul');
-const items = document.getElementsByClassName('task')
+const items = document.getElementsByClassName('task');
 
+
+const removeTask = (e) => {
+    e.target.parentNode.remove();
+    taskCounter.textContent = items.length;
+}
 
 const addTask = (e) => {
     e.preventDefault();
@@ -15,7 +20,8 @@ const addTask = (e) => {
     task.innerHTML = newTaskName + '<button>delete</button>';
     tasksList.appendChild(task);
     inputAdd.value = "";
-    taskCounter.textContent = items.length
+    taskCounter.textContent = items.length;
+    task.querySelector('button').addEventListener('click', removeTask)
 }
 
 
