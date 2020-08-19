072289028df77244c0731a8a56dfe7c2dca12421
Grzegorz <g.juzba@gmail.com>
2020-08-19 13:41:34 +0200

    addTask function added

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+const formAdd = document.querySelector('.add');
 const inputAdd = document.querySelector('.add input');
-const addBtn = document.querySelector('.add button');
 const inputSearch = document.querySelector('.search');
 const taskCounter = document.querySelector('h1 span');
 const tasksList = document.querySelector('ul');
+
+const addTask = (e) => {
+    e.preventDefault();
+    let newTaskName = inputAdd.value;
+    if (newTaskName === "") return;
+    const task = document.createElement('li');
+    task.className = 'task';
+    task.innerHTML = newTaskName + '<button>delete</button>';
+    tasksList.appendChild(task);
+    inputAdd.value = ""
+}
+
+
+formAdd.addEventListener('submit', addTask)
