06ec8433673ee6007cc101ff6a03ad1055e5ffce
Grzegorz <g.juzba@gmail.com>
2020-10-08 18:23:39 +0200

    fixt problem with search input clear after task remove

--- main.js
+++ main.js
@@ -1,104 +1,100 @@
-const formAdd = document.querySelector('.add');
-const inputAdd = document.querySelector('.add input');
-const inputSearch = document.querySelector('.search');
-let taskCounter = document.querySelector('h1 span');
-const tasksList = document.querySelector('ul');
-const items = document.getElementsByClassName('task');
-const hourDisp = document.querySelector('.hour span');
-const dateDisp = document.querySelector('.date span');
+const formAdd = document.querySelector(".add");
+const inputAdd = document.querySelector(".add input");
+const inputSearch = document.querySelector(".search");
+const inputSearchValue = document.querySelector(".search input");
+let taskCounter = document.querySelector("h1 span");
+const tasksList = document.querySelector("ul");
+const items = document.getElementsByClassName("task");
+const hourDisp = document.querySelector(".hour span");
+const dateDisp = document.querySelector(".date span");
 const toDoListArr = [];
-const btnAdd = document.querySelector('.activeInput');
-const btnSearch = document.querySelector('.activeSearch');
-
-
-
+const btnAdd = document.querySelector(".activeInput");
+const btnSearch = document.querySelector(".activeSearch");
 
 const removeTask = (e) => {
-    e.target.parentNode.style.textDecorationLine = "line-through";
-    setTimeout(() => {
-        e.target.parentNode.remove();
-        taskCounter.textContent = toDoListArr.length;
-        const index = e.target.parentNode.dataset.key;
-        toDoListArr.splice(index, 1);
-        taskCounter.textContent = toDoListArr.length;
-        inputSearch.value = "";
-        renderList();
-    }, 700);
-
-}
-
-
-const addTask = (e) => {
-    e.preventDefault();
-    let newTaskName = inputAdd.value;
-    if (newTaskName === "") return;
-    const task = document.createElement('li');
-    task.className = 'task';
-    task.innerHTML = newTaskName + '<input type="checkbox">';
-    toDoListArr.push(task);
-    tasksList.appendChild(task);
+  e.target.parentNode.style.textDecorationLine = "line-through";
+  setTimeout(() => {
+    e.target.parentNode.remove();
+    taskCounter.textContent = toDoListArr.length;
+    const index = e.target.parentNode.dataset.key;
+    toDoListArr.splice(index, 1);
+    taskCounter.textContent = toDoListArr.length;
+    inputSearchValue.value = "";
     renderList();
-    inputAdd.value = "";
-    taskCounter.textContent = items.length;
-    task.querySelector('input').addEventListener('click', removeTask);
-}
-
-const searchTask = (e) => {
-    const serachTask = e.target.value.toLowerCase();
-    let tasks = toDoListArr;
-    tasks = tasks.filter(li =>
-        li.textContent.toLocaleLowerCase().includes(serachTask)
-    );
-    tasksList.textContent = "";
-    tasks.forEach(li => tasksList.appendChild(li));
-    console.log(tasks);
-    taskCounter.textContent = items.length;
-    console.log(inputSearch.value)
-
-
-}
-const renderList = () => {
-    tasksList.textContent = "";
-    toDoListArr.forEach((toDoElem, key) => {
-        toDoElem.dataset.key = key;
-        tasksList.appendChild(toDoElem);
-    });
+  }, 700);
 };
 
-inputSearch.addEventListener('input', searchTask);
+const addTask = (e) => {
+  e.preventDefault();
+  let newTaskName = inputAdd.value;
+  if (newTaskName === "") return;
+  const task = document.createElement("li");
+  task.className = "task";
+  task.innerHTML = newTaskName + '<input type="checkbox">';
+  toDoListArr.push(task);
+  tasksList.appendChild(task);
+  renderList();
+  inputAdd.value = "";
+  taskCounter.textContent = items.length;
+  task.querySelector("input").addEventListener("click", removeTask);
+};
 
-formAdd.addEventListener('submit', addTask)
+const searchTask = (e) => {
+  const serachTask = e.target.value.toLowerCase();
+  let tasks = toDoListArr;
+  tasks = tasks.filter((li) =>
+    li.textContent.toLocaleLowerCase().includes(serachTask)
+  );
+  tasksList.textContent = "";
+  tasks.forEach((li) => tasksList.appendChild(li));
+  taskCounter.textContent = items.length;
+  console.log(inputSearch.value);
+};
+const renderList = () => {
+  tasksList.textContent = "";
+  toDoListArr.forEach((toDoElem, key) => {
+    toDoElem.dataset.key = key;
+    tasksList.appendChild(toDoElem);
+  });
+};
+
+inputSearch.addEventListener("input", searchTask);
+
+formAdd.addEventListener("submit", addTask);
 
 // time and date
+
 const clock = () => {
-    const time = new Date();
+  const time = new Date();
 
-    let seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
-    let minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
-    let hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
-    let year = time.getFullYear();
-    const month = time.toLocaleString('default', {
-        month: 'short'
-    })
+  let seconds =
+    time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+  let minutes =
+    time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+  let hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
+  let year = time.getFullYear();
+  const month = time.toLocaleString("default", {
+    month: "short",
+  });
 
-    hourDisp.textContent = `${hours}:${minutes}:${seconds}`
-    dateDisp.textContent = `${time.getDate()} ${month.toUpperCase()} ${year}`
+  hourDisp.textContent = `${hours}:${minutes}:${seconds}`;
+  dateDisp.textContent = `${time.getDate()} ${month.toUpperCase()} ${year}`;
+};
+setInterval(clock, 1000);
 
-}
-setInterval(clock, 1000)
-
+// add/search button function
 const addTaskInputActivator = () => {
-    formAdd.classList.toggle("active");
-    inputSearch.classList.add("active");
-    btnAdd.classList.toggle("animation");
-}
+  formAdd.classList.toggle("active");
+  inputSearch.classList.add("active");
+  btnAdd.classList.toggle("animation");
+};
 
-btnAdd.addEventListener('click', addTaskInputActivator)
+btnAdd.addEventListener("click", addTaskInputActivator);
 
 const searchTaskInputActivator = () => {
-    inputSearch.classList.toggle("active")
-    formAdd.classList.add("active");
-    btnAdd.classList.remove("animation");
-}
+  inputSearch.classList.toggle("active");
+  formAdd.classList.add("active");
+  btnAdd.classList.remove("animation");
+};
 
-btnSearch.addEventListener('click', searchTaskInputActivator)
+btnSearch.addEventListener("click", searchTaskInputActivator);
